719b786eb752f9c26731d6eda3fa4af8a4f207fb
dnsvTheCreator <sergeydenisovcad@gmail.com>
2019-09-22 13:09:52 +0300

    improvements uploaded

--- presentation/src/main/java/com/test/revolutcurrencyconverter/fragment/CurrencyConverterViewModel.kt
+++ presentation/src/main/java/com/test/revolutcurrencyconverter/fragment/CurrencyConverterViewModel.kt
@@ -1,20 +1,26 @@
 package com.test.revolutcurrencyconverter.fragment
 
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import android.provider.SyncStateContract.Helpers.update
+import androidx.lifecycle.*
 import com.test.revolutcurrenciesconverter.LoadCurrenciesUseCase
+import com.test.revolutcurrenciesconverter.PresentationRatesObject
 
-class CurrencyConverterViewModel(private val currenciesUseCase: LoadCurrenciesUseCase) : ViewModel() {
+class CurrencyConverterViewModel(private val currenciesUseCase: LoadCurrenciesUseCase) :
+    ViewModel() {
     private val loadRatesTrigger = MutableLiveData<RatesRequestData>()
+    private val loadRatesResult = loadRatesTrigger.switchMap {
+        currenciesUseCase.execute(it.baseCurrency, it.baseAmount)
+    }
 
-    fun loadRates(baseCurrency: String = BASE_CURRENCY_STRING, baseAmount: Int = BASE_AMOUNT) {
+
+    fun loadRates(baseCurrency: String = BASE_CURRENCY_STRING, baseAmount: Float = BASE_AMOUNT) {
         loadRatesTrigger.postValue(RatesRequestData(baseCurrency, baseAmount))
     }
 
-    data class RatesRequestData(val baseCurrency: String, val baseAmount: Int)
+    data class RatesRequestData(val baseCurrency: String, val baseAmount: Float)
 
     companion object {
         private const val BASE_CURRENCY_STRING = "EUR"
-        private const val BASE_AMOUNT = 100
+        private const val BASE_AMOUNT = 100.0F
     }
 }
