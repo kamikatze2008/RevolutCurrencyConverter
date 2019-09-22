9641a990a8bcacbfbb134e7142271bb97411a651
dnsvTheCreator <sergeydenisovcad@gmail.com>
2019-09-22 12:49:08 +0300

    small improvements made

--- data/src/main/java/com/test/revolutcurrencyconverter/data/RepositoryImpl.kt
+++ data/src/main/java/com/test/revolutcurrencyconverter/data/RepositoryImpl.kt
@@ -3,8 +3,7 @@ package com.test.revolutcurrencyconverter.data
 import com.test.revolutcurrenciesconverter.DomainRatesObject
 import com.test.revolutcurrenciesconverter.Repository
 
-class RepositoryImpl(private val dataSource: DataSource) :
-    Repository {
+class RepositoryImpl(private val dataSource: DataSource) : Repository {
     override suspend fun getLatestRates(base: String): DomainRatesObject {
         return try {
             val response = dataSource.getLatestRates(base)
