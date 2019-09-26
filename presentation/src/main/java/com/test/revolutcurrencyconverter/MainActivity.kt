a2a0d42db0c384add80d23f80c8b69db26446e46
dnsvTheCreator <sergeydenisovcad@gmail.com>
2019-09-26 23:33:21 +0300

    improved fragment states

--- presentation/src/main/java/com/test/revolutcurrencyconverter/MainActivity.kt
+++ presentation/src/main/java/com/test/revolutcurrencyconverter/MainActivity.kt
@@ -6,16 +6,25 @@ import com.test.revolutcurrencyconverter.fragment.CurrencyConverterFragment
 
 class MainActivity : AppCompatActivity() {
 
+    var mainFragment: CurrencyConverterFragment? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        supportFragmentManager.beginTransaction()
-            .add(
-                R.id.container,
-                CurrencyConverterFragment.newInstance(),
-                CurrencyConverterFragment.TAG
-            )
-            .commitNow()
+        mainFragment = if (savedInstanceState == null) {
+
+            CurrencyConverterFragment.newInstance().apply {
+                supportFragmentManager.beginTransaction()
+                    .add(
+                        R.id.container,
+                        this,
+                        CurrencyConverterFragment.TAG
+                    )
+                    .commitNow()
+            }
+        } else {
+            supportFragmentManager.findFragmentById(R.id.container) as CurrencyConverterFragment?
+        }
     }
 }
